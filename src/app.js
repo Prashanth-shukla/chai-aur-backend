3a85b4bc46e9f632aaf35f1268219aabe4417eb1
Prashanth-shukla <147225621+Prashanth-shukla@users.noreply.github.com>
2024-08-11 15:20:46 +0530

    Add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from express
+import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 const app=express()
@@ -12,4 +12,18 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes 
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+
+
+app.use("/api/v1/users",userRouter)
+
+
+//http://localhost:8000/api/v1/users/register
+
+
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,11 @@
- const asynkHandler=()=>{}
+const asyncHandler = (requestHandler) => {
+   return (req, res, next) => {
+       Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+   }
+}
 
- (req,res,next)=>{
-    Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
- }
 
-export {asynkHandler}
+export {asyncHandler}
 // const asynkHandler=(fn)=>async(req,res,next)=>{
 //     try{
 // await fn(req,res,next)
